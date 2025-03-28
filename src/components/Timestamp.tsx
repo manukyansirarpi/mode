7043f2fb6fbbd07809146ca5bffb01a980ba0b48
Sirarpi Manukyan <manukyansirarpi@gmail.com>
2025-03-28 14:17:51 +0400

    Publick static pages

---
+++ src/components/Timestamp.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { useState, useLayoutEffect } from 'react'
+
+export function Timestamp() {
+  const [time, setTime] = useState<number | null>(null)
+  useLayoutEffect(() => {
+    setTime(new Date().getFullYear())
+  }, [])
+  if (time) {
+    return time
+  }
+  return null
+}
